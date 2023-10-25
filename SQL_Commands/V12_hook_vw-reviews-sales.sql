b3d5258079fbd9a741e883788c7ae9d011a897d5
georges2293 <georges.saade22@gmail.com>
2023-10-25 12:11:34 +0300

    added a reviews_sales view

---
+++ SQL_Commands/V12_hook_vw-reviews-sales.sql
@@ -0,0 +1,19 @@
+ CREATE VIEW product_analyzer.reviews_sales AS (
+ SELECT
+    reviews.product_id,
+	products.brand,
+	products.model,
+    reviews.review_date,
+    reviews.total_reviews,
+    reviews.positive_reviews,
+    reviews.positive_percentage,
+    reviews.negative_reviews,
+    reviews.negative_percentage,
+	sales.sales,
+	sales.year
+FROM product_analyzer.agg_reviews AS reviews
+INNER JOIN product_analyzer.dim_product AS products
+ON reviews.product_id=products.product_id
+LEFT OUTER JOIN product_analyzer.fact_sales AS sales
+ON products.brand=sales.brand AND EXTRACT(YEAR FROM reviews.review_date )=sales.year
+)
