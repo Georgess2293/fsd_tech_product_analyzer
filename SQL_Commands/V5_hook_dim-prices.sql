bba46c42f0c89f3a30f6a3695ddd8f294189db6f
georges2293 <georges.saade22@gmail.com>
2023-10-26 00:02:32 +0300

    fixed sql dim_prices

--- SQL_Commands/V5_hook_dim-prices.sql
+++ SQL_Commands/V5_hook_dim-prices.sql
@@ -26,7 +26,7 @@ INSERT INTO product_analyzer.dim_prices
     (product_id,
     _128gb_8gb_ram,
     _128gb_6gb_ram,
-    _256gb_6gb_ram,
+    _256gb_8gb_ram,
     _128gb_12gb_ram,
     _512gb_16gb_ram,
     _256gb_12gb_ram,
@@ -42,22 +42,22 @@ INSERT INTO product_analyzer.dim_prices
     _512gb_8gb_ram)
 SELECT 
 	*
-FROM product_analyzer.stg_products_prices src_prices
+FROM product_analyzer.stg_products_prices1 src_prices
 ON CONFLICT (product_id)
 DO UPDATE SET
-    _128gb_8gb_ram=exclued._128gb_8gb_ram,
-    _128gb_6gb_ram=exclued._128gb_6gb_ram,
-    _256gb_6gb_ram=exclued._256gb_6gb_ram,
-    _128gb_12gb_ram=exclued._128gb_12gb_ram,
-    _512gb_16gb_ram=exclued._512gb_16gb_ram,
-    _256gb_12gb_ram=exclued._256gb_12gb_ram,
-    _32gb_3gb_ram=exclued._32gb_3gb_ram,
-    _64gb_4gb_ram=exclued._64gb_4gb_ram,
-    _128gb_4gb_ram=exclued._128gb_4gb_ram,
-    _512gb_12gb_ram=exclued._512gb_12gb_ram,
-    _64gb_6gb_ram=exclued._64gb_6gb_ram,
-    _256gb_6gb_ram=exclued._256gb_6gb_ram,
-    _512gb_6gb_ram=exclued._512gb_6gb_ram,
-    _256gb_4gb_ram=exclued._256gb_4gb_ram,
-    _512gb_4gb_ram=exclued._512gb_4gb_ram,
-    _512gb_8gb_ram=exclued._512gb_8gb_ram;
+    _128gb_8gb_ram=excluded._128gb_8gb_ram,
+    _128gb_6gb_ram=excluded._128gb_6gb_ram,
+    _256gb_8gb_ram=excluded._256gb_6gb_ram,
+    _128gb_12gb_ram=excluded._128gb_12gb_ram,
+    _512gb_16gb_ram=excluded._512gb_16gb_ram,
+    _256gb_12gb_ram=excluded._256gb_12gb_ram,
+    _32gb_3gb_ram=excluded._32gb_3gb_ram,
+    _64gb_4gb_ram=excluded._64gb_4gb_ram,
+    _128gb_4gb_ram=excluded._128gb_4gb_ram,
+    _512gb_12gb_ram=excluded._512gb_12gb_ram,
+    _64gb_6gb_ram=excluded._64gb_6gb_ram,
+    _256gb_6gb_ram=excluded._256gb_6gb_ram,
+    _512gb_6gb_ram=excluded._512gb_6gb_ram,
+    _256gb_4gb_ram=excluded._256gb_4gb_ram,
+    _512gb_4gb_ram=excluded._512gb_4gb_ram,
+    _512gb_8gb_ram=excluded._512gb_8gb_ram;
