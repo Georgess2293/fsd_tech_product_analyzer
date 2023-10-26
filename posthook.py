b6898dd4e0a0c1e83d494d11cecb8308dbf408ea
georges2293 <georges.saade22@gmail.com>
2023-10-26 19:48:43 +0300

    added execute_posthook

--- posthook.py
+++ posthook.py
@@ -9,5 +9,9 @@ def truncate_staging_tables(schema_name, table_list, db_session):
         TRUNCATE TABLE IF EXISTS {schema_name}.{dst_table}"""
         execute_query(db_session, truncate_query)
 
-
+def execute_posthook():
+    print("posthook")
+    db_session = create_connection()
+    tables = misc_handler.return_staging_tables_as_list()
+    truncate_staging_tables(lookups.DESTINATION_SCHEMA.DESTINATION_NAME.value,tables,db_session)
    
