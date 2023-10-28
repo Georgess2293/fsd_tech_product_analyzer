2b084d81312a8adb9a15d35eaa9ae64de333b800
georges2293 <georges.saade22@gmail.com>
2023-10-28 17:20:11 +0300

    added loggers to posthook

--- posthook.py
+++ posthook.py
@@ -2,6 +2,7 @@ from database_handler import execute_query, create_connection,close_connection
 import misc_handler
 import lookups
 import datetime
+import logging
 
 def truncate_staging_tables(schema_name, table_list, db_session):
     for table in table_list:
@@ -11,10 +12,15 @@ def truncate_staging_tables(schema_name, table_list, db_session):
         execute_query(db_session, truncate_query)
 
 def execute_posthook():
-    print("posthook")
+    logging.basicConfig(
+        level=logging.INFO,
+        format="%(asctime)s - %(name)s - %(levelname)s - %(message)s"
+    )
+    logger=logging.getLogger(__name__)
     db_session = create_connection()
     tables = misc_handler.return_stg_tables_as_list()
-    print(datetime.datetime.now().strftime("%I:%M%p on %B %d, %Y"),"executing prehook sql file")
+    #print(datetime.datetime.now().strftime("%I:%M%p on %B %d, %Y"),"executing prehook sql file")
+    logger.info("Truncating staging tables")
     truncate_staging_tables(lookups.DESTINATION_SCHEMA.DESTINATION_NAME.value,tables,db_session)
     close_connection(db_session)
 
