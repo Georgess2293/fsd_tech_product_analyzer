d398b1957948d05e0b7d7422a3fa3142454d9445
georges2293 <georges.saade22@gmail.com>
2023-10-29 21:46:33 +0200

    removed unused functions

--- prehook.py
+++ prehook.py
@@ -39,7 +39,6 @@ def create_sql_staging_tables_reddit(db_session, driver,reddit):
         dst_table = f"stg_reddit_reviews1"
         create_stmt = return_create_statement_from_df(staging_reddit,DESTINATION_SCHEMA.DESTINATION_NAME.value, dst_table)
         execute_query(db_session=db_session, query= create_stmt)
-        #create_sql_staging_table_index(db_session, 'dw_reporting', dst_table, columns[0])
     except Exception as error:
         print(error)
 
@@ -53,7 +52,6 @@ def create_sql_staging_tables_gsm_reviews(db_session, driver):
         dst_table = f"stg_gsm_reviews1"
         create_stmt = return_create_statement_from_df(staging_gsm_reviews,DESTINATION_SCHEMA.DESTINATION_NAME.value, dst_table)
         execute_query(db_session=db_session, query= create_stmt)
-        #create_sql_staging_table_index(db_session, 'dw_reporting', dst_table, columns[0])
     except Exception as error:
         print(error)
 
@@ -66,12 +64,10 @@ def create_sql_staging_tables_specs(db_session, driver):
                 'Comms_WLAN','Comms_Bluetooth','Comms_Positioning','Comms_NFC','Comms_Radio','Comms_USB','Features_Sensors','Battery_Type','Battery_Charging',
                 'Misc_Colors','Misc_Models','Misc_SAR','Main Camera_Quad','Misc_SAR EU','Main Camera_Dual','Comms_Infrared port','Misc_Price','Selfie camera_Dual','Main Camera_Single']
         staging_specs = pd.DataFrame(columns=columns)
-        #staging_specs=misc_handler.return_specs_df(first_time.specs_url.value,driver)
         staging_specs=clean_specs(staging_specs)
         dst_table = f"stg_products_specs1"
         create_stmt = return_create_statement_from_df(staging_specs,DESTINATION_SCHEMA.DESTINATION_NAME.value, dst_table)
         execute_query(db_session=db_session, query= create_stmt)
-        #create_sql_staging_table_index(db_session, 'dw_reporting', dst_table, columns[0])
     except Exception as error:
         print(error)
 
@@ -87,29 +83,15 @@ def create_sql_staging_tables_prices(db_session, driver):
        '_64GB_4GB_RAM', '_128GB_4GB_RAM', '_512GB_12GB_RAM', '_64GB_6GB_RAM',
        '_256GB_6GB_RAM', '_512GB_6GB_RAM', '_256GB_4GB_RAM', '_512GB_4GB_RAM',
        '_512GB_8GB_RAM','_1TB_16GB_RAM']
-        
         staging_prices = pd.DataFrame(columns=columns)
-        #staging_specs=misc_handler.return_specs_df(first_time.specs_url.value,driver)
-        # staging_prices=clean_prices(staging_prices)
         staging_prices[columns_float]=staging_prices[columns_float].astype('float64')
         col=['product_id']
         staging_prices[col]=staging_prices[col].astype('int64')
         dst_table = f"stg_products_prices1"
         create_stmt = return_create_statement_from_df(staging_prices,DESTINATION_SCHEMA.DESTINATION_NAME.value, dst_table)
         execute_query(db_session=db_session, query= create_stmt)
-        #create_sql_staging_table_index(db_session, 'dw_reporting', dst_table, columns[0])
     except Exception as error:
        print(error)
-
-def create_sql_staging_tables_sales(db_session,driver):
-    try:
-        sales_df=misc_handler.return_sales_per_year_prehook(driver)
-        sales_df=clean_sales(sales_df)
-        dst_table = f"stg_sales1"
-        create_stmt = return_create_statement_from_df(sales_df,DESTINATION_SCHEMA.DESTINATION_NAME.value, dst_table)
-        execute_query(db_session=db_session, query= create_stmt)
-    except Exception as error:
-        print(error)
 
 
 def execute_prehook(reddit,sql_command_directory_path = './SQL_Commands'):
@@ -133,7 +115,6 @@ def execute_prehook(reddit,sql_command_directory_path = './SQL_Commands'):
         create_sql_staging_tables_specs(db_session, driver)
         logger.info("Creating prices staging table")
         create_sql_staging_tables_prices(db_session, driver)
-        #create_sql_staging_tables_sales(db_session,driver)
         close_connection(db_session)
         driver.quit()
         logger.info("Prehook Success")
