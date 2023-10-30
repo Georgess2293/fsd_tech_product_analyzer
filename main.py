16618991ab9cc09c1901f2af21f6a595e4476812
georges2293 <georges.saade22@gmail.com>
2023-10-30 11:11:10 +0200

    adjusted reddit api

--- main.py
+++ main.py
@@ -11,13 +11,13 @@ def etl_job(input_text,reddit):
     hook.execute_hook(input_text,reddit)
     posthook.execute_posthook()
 
-# input_text="Xiaomi 13T pro"
-# reddit=praw.Reddit(
-#             client_id="A99udy2Ex7RaoBzW5O3Gdw",
-#             client_secret="jOKXzOzOe9sk-wn-i5a7c4I4zdac4w",
-#             user_agent="my-tech"
-#         )
+input_text="Xiaomi 13T pro"
+reddit=praw.Reddit(
+            client_id="your_client_id",
+            client_secret="your_client_secret",
+            user_agent="your_user_agent"
+        )
 
-# etl_job("Xiaomi 13T Pro",reddit)
+etl_job("Xiaomi 13T Pro",reddit)
     
 
